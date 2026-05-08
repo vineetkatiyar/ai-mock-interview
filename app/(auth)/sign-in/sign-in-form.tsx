58a684f7115ef3872cc020b88446f820813d188d
Vineet Katiyar <vineetkatiyar98@gmail.com>
2026-05-08 16:42:44 +0530

    signin change

--- app/(auth)/sign-in/sign-in-form.tsx
+++ app/(auth)/sign-in/sign-in-form.tsx
@@ -40,7 +40,7 @@ export function SignInForm() {
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       
-      console.log("signin", values);
+      // console.log("signin", values);
 
       const { email, password } = values;
       const userCredential = await signInWithEmailAndPassword(
@@ -48,14 +48,11 @@ export function SignInForm() {
         email,
         password
       );
-
       const idToken = await userCredential.user.getIdToken();
-
       if (!idToken) {
         toast.error("Signin failed");
         return;
       }
-
       await signIn({
         email,
         idToken,
